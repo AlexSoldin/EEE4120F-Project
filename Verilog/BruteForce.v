ce18edac0a18a745166df3957a3a31d6005fd4ef
Alex Soldin <alex.soldin@gmail.com>
2020-06-15 21:12:49 +0200

    Added some comments

--- Verilog/BruteForce.v
+++ Verilog/BruteForce.v
@@ -1,7 +1,7 @@
 module BruteForce(
     input clock,
     input enable,
-    input [7:0] startingPosition, //starting letter
+    input [7:0] startingPosition, //starting letter position
     input [2:0] increment, //amount of letters to increment by
     output reg[7:0] wordLength,
     output reg[127:0] password //the output password
@@ -12,8 +12,14 @@ wire[127:0] tempPassword;
 reg[127:0] finalPassword;//allows for a 16 byte long character string
 
 ASCIICounter a0(clock, enable, startingPosition, increment, tempPassword[7:0], e0);
-ASCIICounter a1(e0, enable, startingPosition, increment, tempPassword[15:8], e1); //clock is the speed of toggle of the wrap bit of previous counter
-ASCIICounter a2(e1, enable, startingPosition, increment, tempPassword[23:16], e2); //clock is the speed of toggle of the wrap bit of previous counter
+/*
+The clock of a1 is the speed of toggle of the wrap bit of a0
+*/
+ASCIICounter a1(e0, enable, startingPosition, increment, tempPassword[15:8], e1); 
+/*
+The clock of a2 is the speed of toggle of the wrap bit of a1
+*/
+ASCIICounter a2(e1, enable, startingPosition, increment, tempPassword[23:16], e2); 
 
 
 always @(posedge clock) begin

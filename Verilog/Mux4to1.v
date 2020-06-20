ad4f71f4c1e1282399e38d42b17d9154fe563863
Akha Manjezi <amanjezi@gmail.com>
2020-06-20 11:12:15 +0200

    formatting

--- Verilog/Mux4to1.v
+++ Verilog/Mux4to1.v
@@ -1,17 +1,18 @@
-module Mux4to1(
-    input [127:0] in1, in2, in3, in4,
-    input [1:0] sel,
-    output reg [127:0] out
-);
-
-always @ (in1 or in2 or in3 or in4 or sel) begin
-    case (sel)
-        2'b00: out = in1;
-        2'b01: out = in2;
-        2'b10: out = in3;
-        2'b11: out = in4; 
-        default: out = 128'bx;
-    endcase
-end
-
-endmodule // 
+module Mux4to1(input [127:0] in1,
+               in2,
+               in3,
+               in4,
+               input [1:0] sel,
+               output reg [127:0] out);
+    
+    always @ (in1 or in2 or in3 or in4 or sel) begin
+        case (sel)
+            2'b00: out   = in1;
+            2'b01: out   = in2;
+            2'b10: out   = in3;
+            2'b11: out   = in4;
+            default: out = 128'bx;
+        endcase
+    end
+    
+endmodule //

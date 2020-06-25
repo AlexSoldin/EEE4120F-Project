c3fca26539ec829315ea13fc85e3c5fa080bfcc7
Akha Manjezi <amanjezi@gmail.com>
2020-06-25 12:34:07 +0200

    changed clock for brute and character count tree

--- Verilog/MD5Controller.v
+++ Verilog/MD5Controller.v
@@ -27,14 +27,22 @@ wire password_hashes_equal;
 reg [0:127] guess_to_compare; //passed into comparator module
 wire [0:7] numCharacters; //length of string being passed into pancham, received from BruteForce
 reg [0:7] numCharactersReg;
-
+reg bruteclk;
+wire sclk;
 Driver driver(clock, enable, reset);
-BruteForce brute(clock, enable, startingPosition, increment, ready, guess, numCharacters); //output of the BruteForce algorithm is our guess
+BruteForce brute(bruteclk, enable, startingPosition, increment, ready, guess, numCharacters); //output of the BruteForce algorithm is our guess
 pancham encrypter(clock, reset, word_in, word_in_width, msg_in_valid, hashed_password, output_valid, encrypter_ready);
 Comparator comp(target_hash, guess_to_compare, enable, clock, equal_valid, password_hashes_equal);
+slowit slow(clock, reset, sclk);
+
 
 always @ (posedge reset) begin
     enable <= 0; //pull enable low when reset
+end
+
+initial begin
+    bruteclk = 0;
+    bruteclk = 1;
 end
 
 always @ (posedge clock) begin 
@@ -42,9 +50,27 @@ always @ (posedge clock) begin
         enable <= 1; //enable all modules if reset is low, driver module manages reset
     end
     if (enable == 1) begin
+        bruteclk <= output_valid;
         if(encrypter_ready==1 && password_hashes_equal==0 && ready==1)begin //is pancham ready for next word, and we're not done
+        if (numCharacters == 8) begin
+            word_in <= guess[120:127];
+        end if (numCharacters == 16) begin
+            word_in <= {guess[112:119],guess[120:127]};
+        end if (numCharacters == 24) begin
+            word_in <= {guess[104:111],guess[112:119],guess[120:127]};
+        end if (numCharacters == 32) begin
+            word_in <= {guess[96:103],guess[104:111],guess[112:119],guess[120:127]};
+        end if (numCharacters == 40) begin
+            word_in <= {guess[88:95],guess[96:103],guess[104:111],guess[112:119],guess[120:127]};
+        end if (numCharacters == 48) begin
+            word_in <= {guess[80:87],guess[88:95],guess[96:103],guess[104:111],guess[112:119],guess[120:127]};
+        end if (numCharacters == 54) begin
+            word_in <= {guess[72:79],guess[80:87],guess[88:95],guess[96:103],guess[104:111],guess[112:119],guess[120:127]};
+        end if (numCharacters == 62) begin
+            word_in <= {guess[64:71],guess[72:79],guess[80:87],guess[88:95],guess[96:103],guess[104:111],guess[112:119],guess[120:127]};
+        end
             word_in_width <= numCharacters; //num characters from BruteForce
-            word_in <= guess; //get the next word, need to handshake with brute force since words were being SKIPPED! (working now)
+            // word_in <=  ; //get the next word, need to handshake with brute force since words were being SKIPPED! (working now)
             ready<=0; //bruteForce has given us a word, now make it idle.
         end
         if(output_valid==1) begin //is the output of pancham valid
